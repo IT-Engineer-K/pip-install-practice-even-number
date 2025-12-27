35a935e5c47fcd55c6e99155682196419e856258
IT-Engineer-K <funaharakoju@gmail.com>
2025-12-27 09:14:23 +0900

    setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 pip install 可能偶数判定プログラム
+
+pip install git+https://github.com/IT-Engineer-K/pip-install-practice-even-number.git
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="is_odd",
+    version="0.1.0",
+    packages=find_packages(),
+)
